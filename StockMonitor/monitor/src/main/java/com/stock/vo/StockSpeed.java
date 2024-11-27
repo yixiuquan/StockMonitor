fb41aff81d7831bb09222d9183d66a3e422c7711
yixiuquan <yixiuquan7@126.com>
2024-11-27 15:10:03 +0800

    增加属性

--- StockMonitor/monitor/src/main/java/com/stock/vo/StockSpeed.java
+++ StockMonitor/monitor/src/main/java/com/stock/vo/StockSpeed.java
@@ -1,17 +1,24 @@
 package com.stock.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockSpeed {
-    private String code;
-    private String name;
-    private double speed;
-
-    public StockSpeed(String code, String name, double speed) {
-        this.code = code;
-        this.name = name;
-        this.speed = speed;
-    }
-
-    public String getCode() { return code; }
-    public String getName() { return name; }
-    public double getSpeed() { return speed; }
+    private String code;// 股票代码
+    private String name;// 股票名称
+    private double inSpeed;// 股票资金流入实时速度
+    // 股票资金流出实时速度
+    private double outSpeed;
+    private double change;                  // 涨跌额
+    private double changePercent;           // 涨跌幅
+    private double volume;                  // 成交量
+    //排名上涨趋势分数
+    private double rankUpTrendScore;
+    //排名下跌趋势分数
+    private double rankDownTrendScore;
 }
+
